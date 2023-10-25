df15f4c090e365418f74b033b6152d7d37c491b5
Maksim Alekseev <mrvalter566@gmail.com>
2023-10-25 18:24:31 +0700

    Setup project

---
+++ MoonEnhancer/MoonEnhancerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonEnhancerApp.swift
+//  MoonEnhancer
+//
+//  Created by Максим Алексеев  on 25.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonEnhancerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
